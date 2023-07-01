fb3179e618aa621c0cd502562d679be04a81d47c
Dmytro Tkachuk <geekhubme@gmail.com>
2023-07-01 12:57:11 +0300

    first commit

---
+++ src/main/java/com/example/carrest/CarRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRestApplication.class, args);
+    }
+
+}
